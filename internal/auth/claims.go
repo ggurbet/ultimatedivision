0962110b7cddd0fedc9e5bbb595e5ab907dae447
crawter <crawter@hotmail.com>
2021-07-12 16:32:14 +0300

    internal/auth: auth, hmac and cookies functionality added

---
+++ internal/auth/claims.go
@@ -0,0 +1,41 @@
+// Copyright (C) 2021 Creditor Corp. Group.
+// See LICENSE for copying information.
+
+package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	"github.com/zeebo/errs"
+)
+
+// ClaimsError is an error class for Claims errors.
+var ClaimsError = errs.Class("admin auth claims error")
+
+// Claims represents data signed by server and used for authentication.
+type Claims struct {
+	Email     string    `json:"email"`
+	ExpiresAt time.Time `json:"expiresAt"`
+}
+
+// JSON returns json representation of Claims.
+func (c *Claims) JSON() ([]byte, error) {
+	buffer := bytes.NewBuffer(nil)
+
+	err := json.NewEncoder(buffer).Encode(c)
+	return buffer.Bytes(), ClaimsError.Wrap(err)
+}
+
+// FromJSON returns Claims instance, parsed from JSON.
+func FromJSON(data []byte) (*Claims, error) {
+	claims := new(Claims)
+
+	err := json.NewDecoder(bytes.NewReader(data)).Decode(claims)
+	if err != nil {
+		return nil, ClaimsError.Wrap(err)
+	}
+
+	return claims, nil
+}
