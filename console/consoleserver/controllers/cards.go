02028df61d5b7184bec0745c73e64c2598fcf305
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-08-19 20:43:32 +0300

    console/consoleserver: added ListByPlayerName route and changed filrers mapping

--- console/consoleserver/controllers/cards.go
+++ console/consoleserver/controllers/cards.go
@@ -6,11 +6,13 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/zeebo/errs"
 
 	"ultimatedivision/cards"
 	"ultimatedivision/internal/logger"
+	"ultimatedivision/pkg/sqlsearchoperators"
 )
 
 var (
@@ -40,17 +42,34 @@ func (controller *Cards) List(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	var cardsList []cards.Card
 	var err error
-	var filters cards.SliceFilters
+	var filters []cards.Filters
 	urlQuery := r.URL.Query()
-	tactics := urlQuery.Get(string(cards.Tactics))
-	minPhysique := urlQuery.Get(string(cards.MinPhysique))
-	maxPhysique := urlQuery.Get(string(cards.MaxPhysique))
-	playerName := urlQuery.Get(string(cards.PlayerName))
 
-	filters.Add(cards.Tactics, tactics)
-	filters.Add(cards.MinPhysique, minPhysique)
-	filters.Add(cards.MaxPhysique, maxPhysique)
-	filters.Add(cards.PlayerName, playerName)
+	for key, value := range urlQuery {
+		for k, v := range sqlsearchoperators.SearchOperators {
+			name := key
+			action := sqlsearchoperators.EQ
+
+			if strings.HasSuffix(key, k) {
+				countName := len(key) - 1 + len(k)
+				name = key[:countName]
+				action = v
+			}
+
+			keyFilter := cards.Filter(key)
+			if keyFilter == cards.FilterQuality || keyFilter == cards.FilterDominantFoot || keyFilter == cards.FilterType {
+				action = sqlsearchoperators.EQ
+			}
+
+			filter := cards.Filters{
+				Name:           cards.Filter(name),
+				Value:          value[0],
+				SearchOperator: action,
+			}
+			filters = append(filters, filter)
+			break
+		}
+	}
 
 	if len(filters) > 0 {
 		cardsList, err = controller.cards.ListWithFilters(ctx, filters)
@@ -67,4 +86,32 @@ func (controller *Cards) List(w http.ResponseWriter, r *http.Request) {
 		controller.log.Error("failed to write json response", ErrCards.Wrap(err))
 		return
 	}
+}
+
+// ListByPlayerName is an endpoint that allows will view cards by player name.
+func (controller *Cards) ListByPlayerName(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var filter cards.Filters
+	playerName := r.URL.Query().Get(string(cards.FilterPlayerName))
+	if playerName != "" {
+		filter = cards.Filters{
+			Name:           cards.FilterPlayerName,
+			Value:          playerName,
+			SearchOperator: sqlsearchoperators.LIKE,
+		}
+	}
+
+	cardsList, err := controller.cards.ListByPlayerName(ctx, filter)
+	if err != nil {
+		controller.log.Error("could not get cards list", ErrCards.Wrap(err))
+		http.Error(w, "could not get cards list", http.StatusInternalServerError)
+		return
+	}
+
+	if err = json.NewEncoder(w).Encode(cardsList); err != nil {
+		controller.log.Error("failed to write json response", ErrCards.Wrap(err))
+		return
+	}
+
 }
