42e2dfd0f8d1dfa677581cb6aa0a694122a47d05
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-08-19 20:38:39 +0300

    cards/cards: added Type field, changed Filters struct and Filter type, changed Validate method for Filters

--- cards/cards.go
+++ cards/cards.go
@@ -5,12 +5,13 @@ package cards
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/google/uuid"
 	"github.com/zeebo/errs"
+
+	"ultimatedivision/pkg/sqlsearchoperators"
 )
 
 // ErrNoCard indicated that card does not exist.
@@ -31,6 +32,8 @@ type DB interface {
 	List(ctx context.Context) ([]Card, error)
 	// ListWithFilters returns all cards from the data base with filters.
 	ListWithFilters(ctx context.Context, filters []Filters) ([]Card, error)
+	// ListByPlayerName returns cards from DB by player name.
+	ListByPlayerName(ctx context.Context, filters Filters) ([]Card, error)
 	// UpdateStatus updates status card in the database.
 	UpdateStatus(ctx context.Context, id uuid.UUID, status Status) error
 	// UpdateUserID updates user id card in the database.
@@ -54,6 +57,7 @@ type Card struct {
 	DominantFoot     DominantFoot `json:"dominantFoot"`
 	IsTattoos        bool         `json:"isTattoos"`
 	Status           Status       `json:"status"`
+	Type             Type         `json:"type"`
 	UserID           uuid.UUID    `json:"userId"`
 	Tactics          int          `json:"tactics"`
 	Positioning      int          `json:"positioning"`
@@ -170,6 +174,16 @@ const (
 	StatusSale Status = 1
 )
 
+// Type defines the list of possible card Typees.
+type Type string
+
+const (
+	// TypeWon indicates that the card won in a lootbox.
+	TypeWon Type = "won"
+	// TypeBought indicates that the card bought on the marketplaced.
+	TypeBought Type = "bought"
+)
+
 // RangeValueForSkills defines the list of possible group skills.
 var RangeValueForSkills = map[string][]int{}
 
@@ -252,76 +266,192 @@ type PercentageQualities struct {
 }
 
 // Filters entity for using filter cards.
-type Filters map[Filter]string
+type Filters struct {
+	Name           Filter
+	Value          string
+	SearchOperator sqlsearchoperators.SearchOperator
+}
 
 // Filter defines the list of possible filters.
 type Filter string
 
 const (
-	// Tactics indicates an assessment of the card's tactics.
-	Tactics Filter = "tactics"
-	// MinPhysique indicates an assessment of the card's minimum physique.
-	MinPhysique Filter = "min_physique"
-	// MaxPhysique indicates an assessment of the card's maximum physique.
-	MaxPhysique Filter = "max_physique"
-	// Physique indicates an assessment of the card's physique.
-	Physique Filter = "physique"
-	// PlayerName indicates the name of the card player name.
-	PlayerName Filter = "player_name"
+	// FilterTactics indicates an tactics of the card.
+	FilterTactics Filter = "tactics"
+	// FilterPositioning indicates an positioning of the card.
+	FilterPositioning Filter = "positioning"
+	// FilterComposure indicates an composure of the card.
+	FilterComposure Filter = "composure"
+	// FilterAggression indicates an aggression of the card.
+	FilterAggression Filter = "aggression"
+	// FilterVision indicates an vision of the card.
+	FilterVision Filter = "vision"
+	// FilterAwareness indicates an awareness of the card.
+	FilterAwareness Filter = "awareness"
+	// FilterCrosses indicates an crosses of the card.
+	FilterCrosses Filter = "crosses"
+	// FilterPhysique indicates an physique of the card.
+	FilterPhysique Filter = "physique"
+	// FilterAcceleration indicates an acceleration of the card.
+	FilterAcceleration Filter = "acceleration"
+	// FilterRunningSpeed indicates an runningSpeed of the card.
+	FilterRunningSpeed Filter = "runningSpeed"
+	// FilterReactionSpeed indicates an reactionSpeed of the card.
+	FilterReactionSpeed Filter = "reactionSpeed"
+	// FilterAgility indicates an agility of the card.
+	FilterAgility Filter = "agility"
+	// FilterStamina indicates an stamina of the card.
+	FilterStamina Filter = "stamina"
+	// FilterStrength indicates an strength of the card.
+	FilterStrength Filter = "strength"
+	// FilterJumping indicates an jumping of the card.
+	FilterJumping Filter = "jumping"
+	// FilterBalance indicates an balance of the card.
+	FilterBalance Filter = "balance"
+	// FilterTechnique indicates an technique of the card.
+	FilterTechnique Filter = "technique"
+	// FilterDribbling indicates an dribbling of the card.
+	FilterDribbling Filter = "dribbling"
+	// FilterBallControl indicates an ballControl of the card.
+	FilterBallControl Filter = "ballControl"
+	// FilterWeakFoot indicates an weakFoot of the card.
+	FilterWeakFoot Filter = "weakFoot"
+	// FilterSkillMoves indicates an skillMoves of the card.
+	FilterSkillMoves Filter = "skillMoves"
+	// FilterFinesse indicates an finesse of the card.
+	FilterFinesse Filter = "finesse"
+	// FilterCurve indicates an curve of the card.
+	FilterCurve Filter = "curve"
+	// FilterVolleys indicates an volleys of the card.
+	FilterVolleys Filter = "volleys"
+	// FilterShortPassing indicates an shortPassing of the card.
+	FilterShortPassing Filter = "shortPassing"
+	// FilterLongPassing indicates an longPassing of the card.
+	FilterLongPassing Filter = "longPassing"
+	// FilterForwardPass indicates an forwardPass of the card.
+	FilterForwardPass Filter = "forwardPass"
+	// FilterOffense indicates an offense of the card.
+	FilterOffense Filter = "offense"
+	// FilterFinishingAbility indicates an finishingAbility of the card.
+	FilterFinishingAbility Filter = "finishingAbility"
+	// FilterShotPower indicates an shotPower of the card.
+	FilterShotPower Filter = "shotPower"
+	// FilterAccuracy indicates an accuracy of the card.
+	FilterAccuracy Filter = "accuracy"
+	// FilterDistance indicates an distance of the card.
+	FilterDistance Filter = "distance"
+	// FilterPenalty indicates an penalty of the card.
+	FilterPenalty Filter = "penalty"
+	// FilterFreeKicks indicates an freeKicks of the card.
+	FilterFreeKicks Filter = "freeKicks"
+	// FilterCorners indicates an corners of the card.
+	FilterCorners Filter = "corners"
+	// FilterHeadingAccuracy indicates an headingAccuracy of the card.
+	FilterHeadingAccuracy Filter = "headingAccuracy"
+	// FilterDefence indicates an defence of the card.
+	FilterDefence Filter = "defence"
+	// FilterOffsideTrap indicates an offsideTrap of the card.
+	FilterOffsideTrap Filter = "offsideTrap"
+	// FilterSliding indicates an sliding of the card.
+	FilterSliding Filter = "sliding"
+	// FilterTackles indicates an tackles of the card.
+	FilterTackles Filter = "tackles"
+	// FilterBallFocus indicates an ballFocus of the card.
+	FilterBallFocus Filter = "ballFocus"
+	// FilterInterceptions indicates an interceptions of the card.
+	FilterInterceptions Filter = "interceptions"
+	// FilterVigilance indicates an vigilance of the card.
+	FilterVigilance Filter = "vigilance"
+	// FilterGoalkeeping indicates an goalkeeping of the card.
+	FilterGoalkeeping Filter = "goalkeeping"
+	// FilterReflexes indicates an reflexes of the card.
+	FilterReflexes Filter = "reflexes"
+	// FilterDiving indicates an diving of the card.
+	FilterDiving Filter = "diving"
+	// FilterHandling indicates an handling of the card.
+	FilterHandling Filter = "handling"
+	// FilterSweeping indicates an sweeping of the card.
+	FilterSweeping Filter = "sweeping"
+	// FilterThrowing indicates an throwing of the card.
+	FilterThrowing Filter = "throwing"
+	// FilterQuality indicates an quality of the card.
+	FilterQuality Filter = "quality"
+	// FilterHeight indicates an height of the card.
+	FilterHeight Filter = "height"
+	// FilterWeight indicates an weight of the card.
+	FilterWeight Filter = "weight"
+	// FilterDominantFoot indicates an dominant foot of the card.
+	FilterDominantFoot Filter = "dominantFoot"
+	// FilterType indicates an type of the card.
+	FilterType Filter = "type"
+	// FilterPlayerName indicates the name of the card player name.
+	FilterPlayerName Filter = "player_name"
+	// FilterPrice indicates an price of the card.
+	// FilterPrice Filter = "price".
 )
-
-// SliceFilters entity for using group filter cards.
-type SliceFilters []Filters
-
-// Add check is empty and append value to slice.
-func (s *SliceFilters) Add(name Filter, value string) {
-	if value == "" {
-		return
-	}
-
-	filter := Filters{
-		name: value,
-	}
-	*s = append(*s, filter)
-}
 
 // Validate check of valid UTF-8 bytes and type.
 func (f Filters) Validate() error {
-	if _, found := f[Tactics]; found == true {
-		strings.ToValidUTF8(f[Tactics], "")
+	if f.Name == FilterTactics || f.Name == FilterPositioning || f.Name == FilterComposure || f.Name == FilterAggression ||
+		f.Name == FilterVision || f.Name == FilterAwareness || f.Name == FilterCrosses || f.Name == FilterPhysique ||
+		f.Name == FilterAcceleration || f.Name == FilterRunningSpeed || f.Name == FilterReactionSpeed || f.Name == FilterAgility ||
+		f.Name == FilterStamina || f.Name == FilterStrength || f.Name == FilterJumping || f.Name == FilterBalance ||
+		f.Name == FilterTechnique || f.Name == FilterDribbling || f.Name == FilterBallControl || f.Name == FilterWeakFoot ||
+		f.Name == FilterSkillMoves || f.Name == FilterFinesse || f.Name == FilterCurve || f.Name == FilterVolleys ||
+		f.Name == FilterShortPassing || f.Name == FilterLongPassing || f.Name == FilterForwardPass || f.Name == FilterOffense ||
+		f.Name == FilterFinishingAbility || f.Name == FilterShotPower || f.Name == FilterAccuracy || f.Name == FilterDistance ||
+		f.Name == FilterPenalty || f.Name == FilterFreeKicks || f.Name == FilterCorners || f.Name == FilterHeadingAccuracy ||
+		f.Name == FilterDefence || f.Name == FilterOffsideTrap || f.Name == FilterSliding || f.Name == FilterTackles ||
+		f.Name == FilterBallFocus || f.Name == FilterInterceptions || f.Name == FilterVigilance || f.Name == FilterGoalkeeping ||
+		f.Name == FilterReflexes || f.Name == FilterDiving || f.Name == FilterHandling || f.Name == FilterSweeping || f.Name == FilterThrowing {
+		strings.ToValidUTF8(f.Value, "")
 
-		_, err := strconv.Atoi(f[Tactics])
+		_, err := strconv.Atoi(f.Value)
 		if err != nil {
-			return ErrInvalidFilter.Wrap(fmt.Errorf("%s %s", f[Tactics], err))
+			return ErrInvalidFilter.New("%s %s", f.Value, err)
 		}
+		return nil
 	}
 
-	if _, found := f[MinPhysique]; found == true {
-		strings.ToValidUTF8(f[MinPhysique], "")
+	if f.Name == FilterHeight || f.Name == FilterWeight { // || f.Name == FilterPrice
+		strings.ToValidUTF8(f.Value, "")
 
-		_, err := strconv.Atoi(f[MinPhysique])
+		_, err := strconv.ParseFloat(f.Value, 64)
 		if err != nil {
-			return ErrInvalidFilter.Wrap(fmt.Errorf("%s %s", f[MinPhysique], err))
+			return ErrInvalidFilter.New("%s %s", f.Value, err)
 		}
+		return nil
 	}
 
-	if _, found := f[MaxPhysique]; found == true {
-		strings.ToValidUTF8(f[MaxPhysique], "")
+	if f.Name == FilterQuality {
+		strings.ToValidUTF8(f.Value, "")
 
-		_, err := strconv.Atoi(f[MaxPhysique])
-		if err != nil {
-			return ErrInvalidFilter.Wrap(fmt.Errorf("%s %s", f[MaxPhysique], err))
+		quality := Quality(f.Value)
+		if quality == QualityWood || quality == QualitySilver || quality == QualityGold || quality == QualityDiamond {
+			return nil
 		}
+		return ErrInvalidFilter.New("%s %s", f.Value, " is not an indicator of quality card")
 	}
 
-	if _, found := f[PlayerName]; found == true {
-		strings.ToValidUTF8(f[PlayerName], "")
+	if f.Name == FilterDominantFoot {
+		strings.ToValidUTF8(f.Value, "")
 
-		_, err := strconv.Atoi(f[PlayerName])
-		if err == nil {
-			return ErrInvalidFilter.Wrap(fmt.Errorf("%s %s", f[PlayerName], err))
+		dominantFoot := DominantFoot(f.Value)
+		if dominantFoot == DominantFootLeft || dominantFoot == DominantFootRight {
+			return nil
 		}
+		return ErrInvalidFilter.New("%s %s", f.Value, " is not an indicator of dominant foot card")
 	}
 
-	return nil
+	if f.Name == FilterType {
+		strings.ToValidUTF8(f.Value, "")
+
+		filterType := Type(f.Value)
+		if filterType == TypeWon || filterType == TypeBought {
+			return nil
+		}
+		return ErrInvalidFilter.New("%s %s", f.Value, " is not an indicator of type card")
+	}
+
+	return ErrInvalidFilter.New("invalid name parameter - " + string(f.Name))
 }
