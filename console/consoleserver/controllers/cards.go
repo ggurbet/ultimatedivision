113db79130447075b941b5cd4b1781716af7ac5c
Dmytro <38706023+Dmytro-yakymuk@users.noreply.github.com>
2021-08-31 19:32:03 +0300

    console/consoleserver: changed place search cards by player name (#132)

--- console/consoleserver/controllers/cards.go
+++ console/consoleserver/controllers/cards.go
@@ -52,9 +52,10 @@ func (controller *Cards) List(w http.ResponseWriter, r *http.Request) {
 		err       error
 		filters   []cards.Filters
 	)
-	urlQuery := r.URL.Query()
 
-	if len(urlQuery) > 0 {
+	urlQuery := r.URL.Query()
+	playerName := urlQuery.Get(string(cards.FilterPlayerName))
+	if playerName == "" {
 		for key, value := range urlQuery {
 			filter := cards.Filters{
 				Name:           "",
@@ -83,50 +84,20 @@ func (controller *Cards) List(w http.ResponseWriter, r *http.Request) {
 
 			filters = append(filters, filter)
 		}
-	}
 
-	if len(filters) > 0 {
-		cardsList, err = controller.cards.ListWithFilters(ctx, filters)
-	} else {
-		cardsList, err = controller.cards.List(ctx)
-	}
-	if err != nil {
-		controller.log.Error("could not get cards list", ErrCards.Wrap(err))
-		switch {
-		case userauth.ErrUnauthenticated.Has(err):
-			controller.serveError(w, http.StatusUnauthorized, ErrCards.Wrap(err))
-		case cards.ErrNoCard.Has(err):
-			controller.serveError(w, http.StatusNotFound, ErrCards.Wrap(err))
-		default:
-			controller.serveError(w, http.StatusInternalServerError, ErrCards.Wrap(err))
+		if len(filters) > 0 {
+			cardsList, err = controller.cards.ListWithFilters(ctx, filters)
+		} else {
+			cardsList, err = controller.cards.List(ctx)
 		}
-		return
+	} else {
+		filter := cards.Filters{
+			Name:           cards.FilterPlayerName,
+			Value:          playerName,
+			SearchOperator: sqlsearchoperators.LIKE,
+		}
+		cardsList, err = controller.cards.ListByPlayerName(ctx, filter)
 	}
-
-	if err = json.NewEncoder(w).Encode(cardsList); err != nil {
-		controller.log.Error("failed to write json response", ErrCards.Wrap(err))
-		return
-	}
-}
-
-// ListByPlayerName is an endpoint that allows will view cards by player name.
-func (controller *Cards) ListByPlayerName(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	w.Header().Set("Content-Type", "application/json")
-
-	var filter cards.Filters
-	playerName := r.URL.Query().Get(string(cards.FilterPlayerName))
-	if playerName == "" {
-		controller.serveError(w, http.StatusBadRequest, ErrCards.New("empty player name parameter"))
-		return
-	}
-	filter = cards.Filters{
-		Name:           cards.FilterPlayerName,
-		Value:          playerName,
-		SearchOperator: sqlsearchoperators.LIKE,
-	}
-
-	cardsList, err := controller.cards.ListByPlayerName(ctx, filter)
 	if err != nil {
 		controller.log.Error("could not get cards list", ErrCards.Wrap(err))
 		switch {
