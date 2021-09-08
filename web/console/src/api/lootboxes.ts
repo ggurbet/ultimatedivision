2afe2db1c08fc62f082c56e778b2ed0bdf1c0d8a
Тарас <potsikaylot@gmail.com>
2021-09-08 17:05:11 +0300

    web/console: fixed buy and delete method and types for lootbox (#162)

--- web/console/src/api/lootboxes.ts
+++ web/console/src/api/lootboxes.ts
@@ -6,12 +6,11 @@ import { Lootbox } from '@/lootbox';
 
 /** LootboxClient is a lootbox api client */
 export class LootboxClient extends APIClient {
-    private readonly ROOT_PATH: string = '/lootboxes';
+    private readonly ROOT_PATH: string = '/api/v0/lootboxes';
     /** buys and opens lootbox */
     public async buy(lootbox: Lootbox): Promise<Response> {
-        /** TODO: delete post http method after back-end reworks */
         await this.http.post(this.ROOT_PATH, JSON.stringify({ type: lootbox.type }));
 
-        return await this.http.delete(`${this.ROOT_PATH}/${lootbox.uuid}`);
+        return await this.http.post(`${this.ROOT_PATH}/${lootbox.id}`);
     };
 };
