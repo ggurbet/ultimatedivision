bc4695eb9970898fe6876011658dddd605449860
Oleh Perehuda <57601840+OlehPerehuda@users.noreply.github.com>
2021-10-06 12:15:55 +0300

    deleted devtool property and changed port for devServer (#243)

--- web/console/webpack.config.js
+++ web/console/webpack.config.js
@@ -6,103 +6,102 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const StylelintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  experiments: {
-    asset: true,
-  },
-  entry: "./src/index.tsx",
-  target: "web",
-  devtool: "inline-source-map",
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    filename: "[name].[hash].js",
-    publicPath: "/static/dist/",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Ultimate Division",
-      template: "./public/index.html",
-      favicon: "./src/app/static/img/favicon.ico",
-    }),
-    new CleanWebpackPlugin(),
-    new CssMinimizerPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new StylelintPlugin({ fix: true }),
-  ],
-  devServer: {
-    port: 3001,
-    open: true,
-    historyApiFallback: true,
-    hot: true,
-  },
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "./src/app/components/"),
-      "@static": path.resolve(__dirname, "./src/app/static/"),
-      "@": path.resolve(__dirname, "./src/"),
+    mode: "development",
+    experiments: {
+        asset: true,
     },
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    modules: ["node_modules"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?(tsx|ts)$/i,
-        exclude: /(node_modules)/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(s[c]ss|css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        exclude: /(node_modules)/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][hash:5][ext]",
-        },
-      },
-      {
-        test: /\.(jpe|jpg|png|svg)(\?.*$|$)/,
-        exclude: /(node_modules)/,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name][hash:5][ext]",
-        },
-        use: [
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.8, 0.9],
-                speed: 2,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                enabled: false,
-              },
-            },
-          },
-        ],
-      },
+    entry: "./src/index.tsx",
+    target: "web",
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        filename: "[name].[hash].js",
+        publicPath: "/static/dist/",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Ultimate Division",
+            template: "./public/index.html",
+            favicon: "./src/app/static/img/favicon.ico",
+        }),
+        new CleanWebpackPlugin(),
+        new CssMinimizerPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new StylelintPlugin({ fix: true }),
     ],
-  },
+    devServer: {
+        port: 3000,
+        open: true,
+        historyApiFallback: true,
+        hot: true,
+    },
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "./src/app/components/"),
+            "@static": path.resolve(__dirname, "./src/app/static/"),
+            "@": path.resolve(__dirname, "./src/"),
+        },
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        modules: ["node_modules"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?(tsx|ts)$/i,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(s[c]ss|css)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                exclude: /(node_modules)/,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name][hash:5][ext]",
+                },
+            },
+            {
+                test: /\.(jpe|jpg|png|svg)(\?.*$|$)/,
+                exclude: /(node_modules)/,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[name][hash:5][ext]",
+                },
+                use: [
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.8, 0.9],
+                                speed: 2,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                enabled: false,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
