1c4e0d3c2c0b37d190b98770f29b40db7db33ef6
Vlad Dezhniuk <77901473+vladdezhniuk@users.noreply.github.com>
2021-10-08 14:17:07 +0300

    images changes, file structure changes (#257)

--- web/nftdrop/landing/src/App.tsx
+++ web/nftdrop/landing/src/App.tsx
@@ -1,16 +1,16 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { Roadmap } from '@components/WelcomePage/Roadmap';
-import { Projects } from '@components/WelcomePage/Projects';
-import { Footer } from '@components/WelcomePage/Footer';
-import { LaunchRoadmap } from '@components/WelcomePage/LaunchRoadmap';
-import { Navbar } from '@components/WelcomePage/NavBar';
-import { Home } from '@components/WelcomePage/Home';
-import { LaunchDate } from '@components/WelcomePage/LaunchDate';
-import { Description } from '@components/WelcomePage/Description';
-import { Metaverse } from '@components/WelcomePage/Metaverse';
-import { Authors } from '@components/WelcomePage/Authors';
+import { Roadmap } from '@components/Roadmap';
+import { Projects } from '@components/Projects';
+import { Footer } from '@components/Footer';
+import { LaunchRoadmap } from '@components/LaunchRoadmap';
+import { Navbar } from '@components/NavBar';
+import { Home } from '@components/Home';
+import { LaunchDate } from '@components/LaunchDate';
+import { Description } from '@components/Description';
+import { Metaverse } from '@components/Metaverse';
+import { Authors } from '@components/Authors';
 
 export const App = () => (
     <main className="main">
--- web/nftdrop/landing/webpack.config.js
+++ web/nftdrop/landing/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist/"),
         filename: "[name].[hash].js",
-        publicPath: "/static/dist/",
+        publicPath: "/",
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -90,7 +90,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.(jpe|jpg|png|svg)(\?.*$|$)/,
+                test: /\.(jpe|jpg|png|svg|webp)(\?.*$|$)/,
                 exclude: /(node_modules)/,
                 type: "asset/resource",
                 generator: {
