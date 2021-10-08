271f88e9cc92ba6863717552e8f058a7edc339d5
Тарас <potsikaylot@gmail.com>
2021-10-08 12:55:12 +0300

    web/console: rewrote Information page styles  (#247)

--- web/console/src/app/components/common/AboutMenu/index.tsx
+++ web/console/src/app/components/common/AboutMenu/index.tsx
@@ -18,100 +18,74 @@ export const AboutMenu = () => {
     const tokenomicsStyle = new DropdownStyle(tokenomicsVisibility);
 
     const path = useLocation().pathname;
-    const shouldBeShowed = path.includes('tokenomics') || path.includes('whitepaper');
+    const shouldBeShowed =
+    path.includes('tokenomics') || path.includes('whitepaper');
 
     const menuFields = {
-        whitepaper:
-            [
-                'Summary',
-                'Game Mechanics',
-                'Play to Earn and Economy',
-                'Technology',
-                'Team',
-            ],
-        tokenomics:
-            [
-                'UDT Spending',
-                'Play to Earn',
-                'Staking',
-                'UD DAO Fund',
-            ],
+        whitepaper: [
+            'Summary',
+            'Game Mechanics',
+            'Play to Earn and Economy',
+            'Technology',
+            'Team',
+        ],
+        tokenomics: ['UDT Spending', 'Play to Earn', 'Staking', 'UD DAO Fund'],
     };
 
-    return (
-        shouldBeShowed ?
-            <div className="about-menu">
-                <div className="about-menu__logo-wrapper">
-                    <img src={ultimate} alt="ultimate logo" />
-                </div>
-                <div
-                    className="about-menu__whitepaper"
-                    onClick={() => changeWhitePaperVisibility(prev => !prev)}
-                >
-                    <h2>Whitepaper</h2>
-                    <img
-                        className="about-menu__whitepaper-image"
-                        src={triangle}
-                        style={{ transform: whitePaperStyle.triangleRotate }}
-                        alt="triangle img"
-                    />
-                </div>
-                <ul
-                    className="about-menu__whitepaper-list"
-                    style={{ height: whitePaperStyle.listHeight }}
-                >
-                    {
-                        RouteConfig.Whitepaper.children &&
-                        RouteConfig.Whitepaper.children.map((item, index) =>
-                            <li
-                                key={index}
-                                className="about-menu__whitepaper-item"
-                            >
-                                <Link
-                                    to={item.path}
-                                    className="about-menu__whitepaper-link"
-                                >
-                                    {menuFields.whitepaper[index]}
-                                </Link>
-                            </li>
-                        )
-                    }
-                </ul>
-                <div
-                    className="about-menu__tokenomics"
-                    onClick={() => changeTokenomicsVisibility(prev => !prev)}
-                >
-                    <h2>Tokenomics</h2>
-                    <img
-                        className="about-menu__whitepaper-image"
-                        src={triangle}
-                        style={{ transform: tokenomicsStyle.triangleRotate }}
-                        alt="triangle img"
-                    />
-                </div>
-                <ul
-                    className="about-menu__tokenomics-list"
-                    style={{ height: tokenomicsStyle.listHeight }}
-                >
-                    {
-                        RouteConfig.Tokenomics.children &&
-                        RouteConfig.Tokenomics.children.map((item, index) =>
-                            <li
-                                key={index}
-                                className="about-menu__tokenomics-item"
-                            >
-                                <Link
-                                    to={item.path}
-                                    className="about-menu__tokenomics-link"
-                                >
-                                    {menuFields.tokenomics[index]}
-                                </Link>
-                            </li>
-                        )
-                    }
-                </ul>
+    return shouldBeShowed ?
+        <div className="about-menu">
+            <span className="about-menu__title">Information</span>
+            <div
+                className="about-menu__whitepaper"
+                onClick={() => changeWhitePaperVisibility((prev) => !prev)}
+            >
+                <h2>Gameplay</h2>
+                <img
+                    className="about-menu__whitepaper-image"
+                    src={triangle}
+                    style={{ transform: whitePaperStyle.triangleRotate }}
+                    alt="triangle img"
+                />
             </div>
-            :
-            <></>
-    );
+            <ul
+                className="about-menu__whitepaper-list"
+                style={{ height: whitePaperStyle.listHeight }}
+            >
+                {RouteConfig.Whitepaper.children &&
+          RouteConfig.Whitepaper.children.map((item, index) =>
+              <li key={index} className="about-menu__whitepaper-item">
+                  <Link to={item.path} className="about-menu__whitepaper-link">
+                      {menuFields.whitepaper[index]}
+                  </Link>
+              </li>
+          )}
+            </ul>
+            <div
+                className="about-menu__tokenomics"
+                onClick={() => changeTokenomicsVisibility((prev) => !prev)}
+            >
+                <h2>Community</h2>
+                <img
+                    className="about-menu__whitepaper-image"
+                    src={triangle}
+                    style={{ transform: tokenomicsStyle.triangleRotate }}
+                    alt="triangle img"
+                />
+            </div>
+            <ul
+                className="about-menu__tokenomics-list"
+                style={{ height: tokenomicsStyle.listHeight }}
+            >
+                {RouteConfig.Tokenomics.children &&
+          RouteConfig.Tokenomics.children.map((item, index) =>
+              <li key={index} className="about-menu__tokenomics-item">
+                  <Link to={item.path} className="about-menu__tokenomics-link">
+                      {menuFields.tokenomics[index]}
+                  </Link>
+              </li>
+          )}
+            </ul>
+        </div>
+        :
+        <></>;
 };
