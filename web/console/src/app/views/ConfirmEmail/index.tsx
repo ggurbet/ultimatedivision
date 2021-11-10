6dba86b82e9d663d732cd354093abfda06407c45
Oleh Perehuda <57601840+OlehPerehuda@users.noreply.github.com>
2021-11-10 11:02:36 +0200

    web/console/src/app/views/signIn: height media queries added  (#323)

--- web/console/src/app/views/ConfirmEmail/index.tsx
+++ web/console/src/app/views/ConfirmEmail/index.tsx
@@ -1,7 +1,9 @@
 // Copyright (C) 2021 Creditor Corp. Group.
 // See LICENSE for copying information.
 
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { toast } from 'react-toastify';
 
 import { UserClient } from '@/api/user';
 import { UserService } from '@/user/service';
@@ -9,12 +11,9 @@ import { UserService } from '@/user/service';
 import { useQueryToken } from '@/app/hooks/useQueryToken';
 import { AuthRouteConfig } from '@/app/routes';
 
-/** TODO: Rework this view after design solution */
 const ConfirmEmail: React.FC = () => {
     const token = useQueryToken();
-
-    const [errorMessage, setErrorMessage]
-        = useState<SetStateAction<null | string>>(null);
+    const history = useHistory();
 
     const userClient = new UserClient();
     const users = new UserService(userClient);
@@ -24,13 +23,18 @@ const ConfirmEmail: React.FC = () => {
     async function checkEmailToken() {
         try {
             await users.checkEmailToken(token);
-
+            toast.success(`Your email has been successfully verified.
+            You will be redirected to the sign-in page in 3 seconds.`, {
+                position: toast.POSITION.TOP_RIGHT,
+            });
             await setTimeout(() => {
-                location.pathname = AuthRouteConfig.SignIn.path;
+                history.push(AuthRouteConfig.SignIn.path);
             }, DELAY);
         } catch (error: any) {
-            /** TODO: handles error */
-            setErrorMessage('Email verification failed');
+            toast.error('Email verification failed', {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: 'colored',
+            });
         };
     };
 
@@ -38,16 +42,7 @@ const ConfirmEmail: React.FC = () => {
         checkEmailToken();
     }, []);
 
-    if (errorMessage) {
-        return <h1>{errorMessage}</h1>;
-    };
-
-    return <div>
-        <h1>
-            Your email has been successfully verified.
-            You will be redirected to the sign-in page in 3 seconds.
-        </h1>
-    </div>;
+    return <div className="confirm-email"/>
 };
 
 export default ConfirmEmail;
