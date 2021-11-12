42c1508b862c1636fa560a9745fc401e89bd1867
Oleh Perehuda <57601840+OlehPerehuda@users.noreply.github.com>
2021-11-12 13:10:25 +0200

    web/console/src/app: changed checkbox input (#344)

--- web/console/src/app/views/SignIn/index.tsx
+++ web/console/src/app/views/SignIn/index.tsx
@@ -85,16 +85,6 @@ const SignIn: React.FC = () => {
             clearError: setPasswordError,
             validate: Validator.isPassword,
         },
-        {
-            value: 'Remember me',
-            placeHolder: '',
-            onChange: handleIsRemember,
-            className: 'register__sign-in__sign-form__remember-me',
-            type: 'radio',
-            error: null,
-            clearError: null,
-            validate: () => false,
-        },
     ];
 
     return (
@@ -112,8 +102,9 @@ const SignIn: React.FC = () => {
                     className="register__sign-in__sign-form"
                     onSubmit={handleSubmit}
                 >
-                    {signInDatas.map((data, index) => data.type === 'radio' ? <div key={index}>
+                    {signInDatas.map((data, index) =>
                         <UserDataArea
+                            key={index}
                             value={data.value}
                             placeHolder={data.placeHolder}
                             onChange={data.onChange}
@@ -122,24 +113,26 @@ const SignIn: React.FC = () => {
                             error={data.error}
                             clearError={data.clearError}
                             validate={data.validate}
+                        />)}
+                    <div className="register__sign-in__sign-form__checkbox-wrapper">
+                        <input
+                            id="register-sign-in-checkbox"
+                            className="register__sign-in__sign-form__remember-me"
+                            type="checkbox"
                         />
+                        <label
+                            className="register__sign-in__sign-form__remember-me__text"
+                            htmlFor="register-sign-in-checkbox"
+                        >
+                            Remember me
+                        </label>
                         <Link
                             to={AuthRouteConfig.ChangePassword.path}
                             className="register__sign-in__sign-form__forgot-password"
                         >
                             Forgot Password?
                         </Link>
-                    </div> : <UserDataArea
-                        key={index}
-                        value={data.value}
-                        placeHolder={data.placeHolder}
-                        onChange={data.onChange}
-                        className={data.className}
-                        type={data.type}
-                        error={data.error}
-                        clearError={data.clearError}
-                        validate={data.validate}
-                    />)}
+                    </div>
                     <div className="register__sign-in__sign-form__auth-internal">
                         <input
                             className="register__sign-in__sign-form__confirm"
