98de027aca5b01b7100fb951d9846a4724361e66
Vlad Dezhniuk <77901473+vladdezhniuk@users.noreply.github.com>
2021-11-15 19:46:08 +0200

    web/nftdrop/landing: webpack modes (#358)

--- web/nftdrop/landing/webpack.config.js
+++ web/nftdrop/landing/webpack.config.js
@@ -8,139 +8,153 @@ const zlib = require("zlib");
 const CompressionPlugin = require("compression-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-    mode: "production",
-    experiments: {
-        asset: true,
-    },
-    entry: "./src/index.tsx",
-    target: "web",
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        filename: "[name].[hash].js",
-        publicPath: "/static/dist/",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Ultimate Division",
-            template: "./public/index.html",
-            favicon: "./src/app/static/images/favicon.ico",
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new StylelintPlugin({ fix: true }),
-        new CompressionPlugin({
-            filename: "[path][base].br",
-            algorithm: "brotliCompress",
-            test: /\.(js|css|html|svg)$/,
-            compressionOptions: {
-                params: {
-                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-                },
+const PRODUCTION_PLUGINS = [
+    new HtmlWebpackPlugin({
+        title: "Ultimate Division",
+        template: "./public/index.html",
+        favicon: "./src/app/static/img/favicon.ico",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new StylelintPlugin({ fix: true }),
+    new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: {
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
             },
-            threshold: 10240,
-            minRatio: 0.8,
-            deleteOriginalAssets: false,
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: false,
+    }),
+];
+
+const DEVELOPMENT_PLUGINS = [
+    new HtmlWebpackPlugin({
+        title: "Ultimate Division",
+        template: "./public/index.html",
+        favicon: "./src/app/static/images/favicon.ico",
+    }),
+];
+
+const OPTIMISATION = {
+    splitChunks: {
+        chunks: "all",
+    },
+    minimize: true,
+    minimizer: [
+        new UglifyJsPlugin({
+            test: /\.js(\?.*)?$/i,
         }),
+        new CssMinimizerPlugin(),
     ],
-    devServer: {
-        port: 3000,
-        open: true,
-        historyApiFallback: true,
-    },
-    resolve: {
-        alias: {
-            "@components": path.resolve(__dirname, "./src/app/components/"),
-            "@static": path.resolve(__dirname, "./src/app/static/"),
-            "@utils": path.resolve(__dirname, "./src/app/utils/"),
-            "@": path.resolve(__dirname, "./src/"),
-        },
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-        modules: ["node_modules"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?(tsx|ts)$/i,
-                exclude: /(node_modules)/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(scss)$/,
-                exclude: /(node_modules)/,
-                use: [
-                    //for dev style-loader, for production
-                    // MiniCssExtractPlugin.loader
-                    "style-loader",
-                    // MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(css)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                exclude: /(node_modules)/,
-                type: "asset/resource",
-                generator: {
-                    filename: "fonts/[name][hash:5][ext]",
-                },
-            },
-            {
-                test: /\.(jpe|jpg|png|svg|webp|mp4)(\?.*$|$)/,
-                exclude: /(node_modules)/,
-                type: "asset/resource",
-                generator: {
-                    filename: "images/[name][hash:5][ext]",
-                },
-                use: [
-                    {
-                        loader: "image-webpack-loader",
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.8, 0.9],
-                                speed: 2,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                enabled: false,
-                            },
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-        },
-        // minimize: true,
-        minimizer: [
-            new UglifyJsPlugin({
-                test: /\.js(\?.*)?$/i,
-            }),
-            // new CssMinimizerPlugin(),
-        ],
-    },
 };
+
+const IMAGE_COMPRESSION = [
+    {
+        loader: "image-webpack-loader",
+        options: {
+            mozjpeg: {
+                progressive: false,
+            },
+            optipng: {
+                enabled: true,
+            },
+            pngquant: {
+                quality: [0.8, 0.8],
+                speed: 2,
+            },
+        },
+    },
+];
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        experiments: {
+            asset: true,
+        },
+        entry: "./src/index.tsx",
+        target: "web",
+        output: {
+            path: path.resolve(__dirname, "dist/"),
+            filename: "[name].[hash].js",
+            publicPath: "/static/dist/",
+        },
+        plugins: isProduction ? PRODUCTION_PLUGINS : DEVELOPMENT_PLUGINS,
+        devServer: {
+            port: 3000,
+            open: true,
+            historyApiFallback: true,
+        },
+        resolve: {
+            alias: {
+                "@components": path.resolve(__dirname, "./src/app/components/"),
+                "@static": path.resolve(__dirname, "./src/app/static/"),
+                "@utils": path.resolve(__dirname, "./src/app/utils/"),
+                "@": path.resolve(__dirname, "./src/"),
+            },
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+            modules: ["node_modules"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?(tsx|ts)$/i,
+                    exclude: /(node_modules)/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                        },
+                    ],
+                },
+                {
+                    test: /\.(scss)$/,
+                    exclude: /(node_modules)/,
+                    use: isProduction
+                        ?
+                        [
+                            MiniCssExtractPlugin.loader,
+                            "css-loader",
+                            "sass-loader",
+                        ]
+                        :
+                        [
+                            "style-loader",
+                            "css-loader",
+                            "sass-loader",
+                        ]
+                },
+                {
+                    test: /\.(css)$/,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                    ],
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    exclude: /(node_modules)/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "fonts/[name][hash:5][ext]",
+                    },
+                },
+                {
+                    test: /\.(jpe|jpg|png|svg|webp|mp4)(\?.*$|$)/,
+                    exclude: /(node_modules)/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "images/[name][hash:5][ext]",
+                    },
+                    use: isProduction ? IMAGE_COMPRESSION : [],
+                },
+            ],
+        },
+        optimization: isProduction ? OPTIMISATION : {},
+    };
+}
