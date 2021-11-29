90eeaefa3904c38486b6f81c746af44185157ab7
Vlad Dezhniuk <77901473+vladdezhniuk@users.noreply.github.com>
2021-11-29 12:27:11 +0200

    filter logic fix (#398)

--- web/console/src/app/components/Field/FieldCardSelection/index.tsx
+++ web/console/src/app/components/Field/FieldCardSelection/index.tsx
@@ -30,7 +30,7 @@ export const FieldCardSelection = () => {
     /** Function filters card list each time when we add card on field */
     function getAvailableCards() {
         const squadCardsIds = squadCards.map(card => card.cardId);
-        return cards.filter((card: CardWithStats) => squadCardsIds.includes(card.id));
+        return cards.filter((card: CardWithStats) => !squadCardsIds.includes(card.id));
     };
 
     /** Add card to field, and hide card selection component */
