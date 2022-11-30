d4fc750d0fc115dbfc02d60f58c7de3c061e3031
AnnaTsybel <76568882+AnnaTsybel@users.noreply.github.com>
2022-11-30 14:47:48 +0200

    web/console: change game info text (#627)
    Co-authored-by: Anna Tsybel <anna.tsybel@nure.ua>

--- web/console/src/app/components/home/GameInfo/index.tsx
+++ web/console/src/app/components/home/GameInfo/index.tsx
@@ -11,34 +11,28 @@ export const GameInfo: React.FC = () =>
         <div className="game-info__content" >
             <div className="game-info__content__text game-info__content__text__left" >
                 <h2 className="game-info__title">Game <span className="game-info__title__second-part">info</span></h2>
-                <p className="game-info__text">Through simple, secure, and scalable technology, NEAR empowers
-                        millions to invent and explore new experiences. Business, creativity,
-                        and community are being reimagined for a more sustainable and inclusive
-                        future.
+                <p className="game-info__text">Play football tactical game and compete against other
+                    players on any platform.Our quick turn-based gameplay
+                    allows football fans challenge each other globally.
                 </p>
-                <p className="game-info__text">Through simple, secure, and scalable technology, NEAR empowers millions to
-                        invent and explore new experiences. Business, creativity, and community
-                        are being reimagined for a more sustainable and inclusive future.
+                <p className="game-info__text">You can form squads of your NFT players and try to outsmart
+                    your opponents or you can improve your gameplay and decision-making
+                    on the field. Find you our way to win.
                 </p>
             </div>
-            <img className="game-info__field-player" src={fieldPlayerRight} alt="field-player"/>
+            <img className="game-info__field-player" src={fieldPlayerRight} alt="field-player" />
         </div>
         <div className="game-info__content game-info__content__second-part">
             <img className="game-info__field-player" src={fieldPlayerLeft} alt="field-player" />
             <div className="game-info__content__text game-info__content__text__right">
-                <p className="game-info__text">Through simple, secure, and scalable technology, NEAR empowers
-                        millions to invent and explore new experiences.
-                        Business, creativity, and community are being reimagined
-                        for a more sustainable and inclusive future.
+                <p className="game-info__text">Every week, a global competition cycle updates
+                    your club's standing in one of the division. Try to get promoted
+                    to the Ultimate Division to get the highest weekly rewards.
                 </p>
-                <p className="game-info__text">Through simple, secure, and scalable technology,
-                        NEAR empowers millions to invent and explore new experiences.
-                        Business, creativity, and community are being reimagined for
-                        a more sustainable and inclusive future.
-                </p>
-                <p className="game-info__text">Through simple, secure, and scalable technology, NEAR empowers millions to
-                        invent and explore new experiences. Business, creativity, and community
-                        are being reimagined for a more sustainable and inclusive future.
+                <p className="game-info__text">Use your winnings to upgrade your
+                    team and club's facilities or cash out at any time you like.
+                    Look for the transfer market for lucrative deals and get to experience football
+                    club economy on the blockchain.
                 </p>
             </div>
         </div>
